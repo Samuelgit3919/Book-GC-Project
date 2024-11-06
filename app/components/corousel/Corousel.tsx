eb635710345652c09cf7d9185c9b1e87d8cc0246
samuel sami <saminew3919@gmail.com>
2024-11-06 09:33:14 -0800

    commit

--- app/components/corousel/Corousel.tsx
+++ app/components/corousel/Corousel.tsx
@@ -1,6 +1,6 @@
 // components/Carousel.js
-import React from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
